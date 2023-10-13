d64b32c78727b278572b3966d03e9989b508f578
Vanessa Haefeli <vhaefeli@c2r2s1.42lausanne.ch>
2023-10-13 15:47:53 +0200

    ex04 ok

--- py0/ex04/whatis.py
+++ py0/ex04/whatis.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
-import sys
+import sys as sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Utilisation : python nom_du_script.py <argument>")
-    else:
-        argument = sys.argv[1]
-        ma_fonction(argument)
+    try:
+        assert len(sys.argv) <= 2, "more than one argument is provided"
+        if len(sys.argv) < 2:
+            sys.exit(0)
+        assert sys.argv[1].isdigit(), "argument is not an integer"
+        nb = int(sys.argv[1])
+        if nb % 2:
+            print("I'm Odd.")
+        else:
+            print("I'm Even.")
+    except AssertionError as msg:
+        print("AssertionError: " + str(msg))
