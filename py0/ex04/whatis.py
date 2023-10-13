d6626aab350a5668dd363e8363a4f09df9719ace
Vhaefeli <vhaefeli@student.42lausanne.ch>
2023-10-13 13:40:42 +0200

    home

--- py0/ex04/whatis.py
+++ py0/ex04/whatis.py
@@ -2,4 +2,9 @@
 
 import sys
 
-
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Utilisation : python nom_du_script.py <argument>")
+    else:
+        argument = sys.argv[1]
+        ma_fonction(argument)
